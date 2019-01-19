17d26fce249b2947a76519cbac1f465bef6362de
junichiro.suzuki <junx0401@gmail.com>
2019-01-19 04:05:08 +0000

    add login and user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
+  before_action :require_user_logged_in
+
   def index
     @tasks=Task.all
   end
@@ -8,14 +9,15 @@ class TasksController < ApplicationController
     @task=Task.new
   end
   def create
-    @task=Task.new(task_params)
+    @task=current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success]='Taskが正常に登録されました'
-      redirect_to @task
+      redirect_to root_url
     else 
+      @tasks=current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger]='Taskは登録されませんでした'
-      render :new
+      render 'toppages/index'
     end
   end
   def destroy
@@ -23,7 +25,7 @@ class TasksController < ApplicationController
     @task.destroy
     
     flash[:success]='削除されました'
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   def edit
     
@@ -44,10 +46,18 @@ class TasksController < ApplicationController
   end
   
   private
+  
   def set_task
     @task=Task.find(params[:id])
   end
   def task_params
     params.require(:task).permit(:status,:content)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
